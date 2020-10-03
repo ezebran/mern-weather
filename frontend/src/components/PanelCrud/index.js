fc708c57f3f4ad5e73284062304192a0492e452e
Ezequiel Brandan <ezebran@gmail.com>
2020-10-03 03:48:34 -0300

    panel cities list

--- frontend/src/components/PanelCrud/index.js
+++ frontend/src/components/PanelCrud/index.js
@@ -3,9 +3,55 @@ import './styles.scss';
 
 function PanelCrud() {
   return (
-    <div>
-      <h3>Panel</h3>
-    </div>
+    <section className="panel">
+      <h1>Panel de administración de ciudades</h1>
+      <p>Crea, ve, edita o elimina ciudades y sus climas, las mismas se veran reflejadas automaticamente.</p>
+
+      <div className="cities-list-panel">
+          <a href="#" className="new-city">
+            <span className="icon-add_location_alt"></span>
+            <h2 className="add-city">Nueva ciudad</h2>
+          </a>
+
+
+
+          <ul className="cities-actions">
+
+            <ul className="row-list">
+              <li className="act-city-name">Cordoba</li>
+              <ul className="actions">
+                <li><a href="#" className="edit"><span className="icon-document-edit"></span>Editar</a></li>
+                <li><a href="#" className="delete"><span className="icon-delete"></span>Eliminar</a></li>  
+              </ul>
+            </ul>
+
+            <ul className="row-list">
+              <li className="act-city-name">Sgo del estero</li>
+              <ul className="actions">
+                <li><a href="#" className="edit"><span className="icon-document-edit"></span>Editar</a></li>
+                <li><a href="#" className="delete"><span className="icon-delete"></span>Eliminar</a></li>  
+              </ul>
+            </ul>
+
+            <ul className="row-list">
+              <li className="act-city-name">Termas</li>
+              <ul className="actions">
+                <li><a href="#" className="edit"><span className="icon-document-edit"></span>Editar</a></li>
+                <li><a href="#" className="delete"><span className="icon-delete"></span>Eliminar</a></li>  
+              </ul>
+            </ul>
+
+            <ul className="row-list">
+              <li className="act-city-name">Tucuman</li>
+              <ul className="actions">
+                <li><a href="#" className="edit"><span className="icon-document-edit"></span>Editar</a></li>
+                <li><a href="#" className="delete"><span className="icon-delete"></span>Eliminar</a></li>  
+              </ul>
+            </ul>
+
+          </ul>
+      </div>
+    </section>
   );
 }
 
